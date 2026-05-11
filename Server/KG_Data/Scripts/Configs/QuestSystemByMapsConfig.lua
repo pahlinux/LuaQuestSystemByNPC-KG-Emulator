b6289ec5562c3def41f51e2bdc83c0288558213e
pahlinux <pahlinux@gmail.com>
2026-05-10 21:33:12 -0300

    Update QuestSystemByMapsConfig.lua

--- Server/KG_Data/Scripts/Configs/QuestSystemByMapsConfig.lua
+++ Server/KG_Data/Scripts/Configs/QuestSystemByMapsConfig.lua
@@ -3,35 +3,36 @@
 -------------------
 ---IMPORTANTE: La Config debe ser igual en server y cliente.
 
-QUEST_SYSTEM_SWITCH = 1
+QUEST_SYSTEM_MAPS_SWITCH = 1
 
-QUEST_SYSTEM_ONLY_ACCOUNT = 1 
-QUEST_SYSTEM_REMOVE_RESETS = 0
-QUEST_SYSTEM_REMOVE_MRESETS = 0
-QUEST_SYSTEM_REMOVE_COIN1 = 0
-QUEST_SYSTEM_REMOVE_COIN2 = 0
-QUEST_SYSTEM_REMOVE_COIN3 = 0
-QUEST_SYSTEM_REMOVE_COIN4 = 0
+QUEST_SYSTEM_MAPS_ONLY_ACCOUNT = 1 
+QUEST_SYSTEM_MAPS_REMOVE_RESETS = 0
+QUEST_SYSTEM_MAPS_REMOVE_MRESETS = 0
+QUEST_SYSTEM_MAPS_REMOVE_COIN1 = 0
+QUEST_SYSTEM_MAPS_REMOVE_COIN2 = 0
+QUEST_SYSTEM_MAPS_REMOVE_COIN3 = 0
+QUEST_SYSTEM_MAPS_REMOVE_COIN4 = 0
 
-QUEST_SYSTEM_USE_GREMORY = 0
-QUEST_SYSTEM_SPACE_INVENTORY = 50
+QUEST_SYSTEM_MAPS_USE_GREMORY = 0
+QUEST_SYSTEM_MAPS_SPACE_INVENTORY = 50
 
 -- Packets
-QUEST_SYSTEM_PACKET = 0x04
-QUEST_SYSTEM_PACKET_OPEN_NAME = 'QuestSystemOpen'
-QUEST_SYSTEM_HUD_UPDATE_NAME = 'QuestSystemHUDUpdate'
-QUEST_SYSTEM_PACKET_START_NAME = 'QuestSystemStartQuest'
-QUEST_SYSTEM_PACKET_GET_REWARD_NAME = 'QuestSystemGetReward'
-QUEST_SYSTEM_PACKET_CONTINUE_QUEST_NAME = 'QuestSystemContinueQuest'
-QUEST_SYSTEM_PACKET_ABANDON_NAME = 'QuestSystemAbandon'
+QUEST_SYSTEM_MAPS_PACKET = 0x05
+QUEST_SYSTEM_MAPS_PACKET_OPEN_NAME = 'QuestSystemByMapsOpen'
+QUEST_SYSTEM_MAPS_HUD_UPDATE_NAME = 'QuestSystemByMapsHUDUpdate'
+QUEST_SYSTEM_MAPS_PACKET_START_NAME = 'QuestSystemByMapsStartQuest'
+QUEST_SYSTEM_MAPS_PACKET_GET_REWARD_NAME = 'QuestSystemByMapsGetReward'
+QUEST_SYSTEM_MAPS_PACKET_CONTINUE_QUEST_NAME = 'QuestSystemByMapsContinueQuest'
+QUEST_SYSTEM_MAPS_PACKET_ABANDON_NAME = 'QuestSystemByMapsAbandon'
 QUEST_DEBUG_MODE = false -- Cambiá a true cuando quieras ver los logs
 
 -- Lista de NPCs permitidos para abrir el Quest System
 -- Puedes añadir todos los que quieras: { [748] = true, [746] = true, [500] = true }
-QUEST_SYSTEM_ALLOWED_NPCS = { 
+QUEST_SYSTEM_MAPS_ALLOWED_NPCS = { 
     [748] = true, 
---    [746] = true 
 }
+-- Por si acaso, definimos esta para evitar errores de nil
+QUEST_SYSTEM_MAPS_NPC_CLASS = 0
 ---------------------------------------------------------
 -- [NUEVO] ESTRUCTURA POR MAPA
 ---------------------------------------------------------
@@ -77,20 +78,20 @@ if QKey == nil then
 end
 
 -- Tablas de Requisitos y Recompensas
-QUEST_SYSTEM_REQUIREMENTS_ITEMS = {}
-QUEST_SYSTEM_REQUIREMENTS_MONSTER = {}
-QUEST_SYSTEM_REWARD_ITEMS = {}
-QUEST_SYSTEM_REWARD_COINS = {}
-QUEST_SYSTEM_REWARD_BUFF = {}
-QUEST_SYSTEM_REWARD_EXP = {}
-QUEST_SYSTEM_POINTS_REWARDS = {}
+QUEST_SYSTEM_MAPS_REQUIREMENTS_ITEMS = {}
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER = {}
+QUEST_SYSTEM_MAPS_REWARD_ITEMS = {}
+QUEST_SYSTEM_MAPS_REWARD_COINS = {}
+QUEST_SYSTEM_MAPS_REWARD_BUFF = {}
+QUEST_SYSTEM_MAPS_REWARD_EXP = {}
+QUEST_SYSTEM_MAPS_POINTS_REWARDS = {}
 
 ---------------------------------------------------------
 -- CONFIGURACIÓN MAPA 0 (Lorencia) - Quest 1 (One-Time)
 ---------------------------------------------------------
 local KEY_LR1 = QKey(748, 0, 1)
 
-QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_LR1 ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER[ KEY_LR1 ] = {
     { MonsterIndex = 3, Quantity = 10, MonsterName = 'Spider' },
 }
 --------------------------------------------------------------------------------
@@ -99,10 +100,10 @@ QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_LR1 ] = {
 --Exp Identification: 1 = Normal Exp, 2 = MasterExp 3=LVL-UP 4=Master LVL-UP
 -- EffectTime = time in seconds Example EffectID = 29 Seal Ascencion
 --------------------------------------------------------------------------------
-QUEST_SYSTEM_REWARD_COINS[ KEY_LR1 ] = {
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_LR1 ] = {
     { CoinName = 'Zen', CoinAmount = 10000000, CoinIdentification = 5 },
 }
-QUEST_SYSTEM_REWARD_EXP[ KEY_LR1 ] = {
+QUEST_SYSTEM_MAPS_REWARD_EXP[ KEY_LR1 ] = {
     { ExpId = 3, Amount = 1, ExpName = "LVL UP" },
 }
 ---------------------------------------------------------
@@ -110,16 +111,16 @@ QUEST_SYSTEM_REWARD_EXP[ KEY_LR1 ] = {
 ---------------------------------------------------------
 local KEY_LR2 = QKey(748, 0, 2)
 
-QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_LR2 ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER[ KEY_LR2 ] = {
     { MonsterIndex = 2, Quantity = 10, MonsterName = 'Budge Dragon' },
 }
 --------------------------------------------------------------------------------
 								--Reward--
 --------------------------------------------------------------------------------
-QUEST_SYSTEM_REWARD_COINS[ KEY_LR2 ] = {
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_LR2 ] = {
     { CoinName = 'Zen', CoinAmount = 20000000, CoinIdentification = 5 },
 }
-QUEST_SYSTEM_REWARD_EXP[ KEY_LR2 ] = {
+QUEST_SYSTEM_MAPS_REWARD_EXP[ KEY_LR2 ] = {
     { ExpId = 3, Amount = 1, ExpName = "LVL UP" },
 }
 ---------------------------------------------------------
@@ -127,16 +128,16 @@ QUEST_SYSTEM_REWARD_EXP[ KEY_LR2 ] = {
 ---------------------------------------------------------
 local KEY_LR3 = QKey(748, 0, 3)
 
-QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_LR3 ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER[ KEY_LR3 ] = {
     { MonsterIndex = 14, Quantity = 10, MonsterName = 'Skeleton' },
 }
 --------------------------------------------------------------------------------
 								--Reward--
 --------------------------------------------------------------------------------
-QUEST_SYSTEM_REWARD_COINS[ KEY_LR3 ] = {
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_LR3 ] = {
     { CoinName = 'Zen', CoinAmount = 50000000, CoinIdentification = 5 },
 }
-QUEST_SYSTEM_REWARD_EXP[ KEY_LR3 ] = {
+QUEST_SYSTEM_MAPS_REWARD_EXP[ KEY_LR3 ] = {
     { ExpId = 3, Amount = 1, ExpName = "LVL UP" },
 }
 ---------------------------------------------------------
@@ -144,19 +145,19 @@ QUEST_SYSTEM_REWARD_EXP[ KEY_LR3 ] = {
 ---------------------------------------------------------
 local KEY_LR50 = QKey(748, 0, 50)
 
-QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_LR50 ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER[ KEY_LR50 ] = {
     { MonsterIndex = 43, Quantity = 1, MonsterName = 'Golden Budge Dragon' },
 }
 --------------------------------------------------------------------------------
 								--Reward--
 --------------------------------------------------------------------------------
-QUEST_SYSTEM_REWARD_ITEMS[ KEY_LR50 ] = {
+QUEST_SYSTEM_MAPS_REWARD_ITEMS[ KEY_LR50 ] = {
     { ItemIndex = GET_ITEM(12,30), Level = 0, Op1=0, Op2=0, Life=0, Exc=0, Ancient=0, JoH=0, SockCount=0, ItemTime=0, DaysExpire=0, Flag=0, Name='Jewel Of Chaos x1', Count=1, Class = -1 }
 }
-QUEST_SYSTEM_REWARD_COINS[ KEY_LR50 ] = {
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_LR50 ] = {
     { CoinName = 'Zen', CoinAmount = 50000000, CoinIdentification = 5 },
 }
-QUEST_SYSTEM_REWARD_EXP[ KEY_LR50 ] = {
+QUEST_SYSTEM_MAPS_REWARD_EXP[ KEY_LR50 ] = {
     { ExpId = 3, Amount = 1, ExpName = "LVL UP" },
 }
 
@@ -165,14 +166,16 @@ QUEST_SYSTEM_REWARD_EXP[ KEY_LR50 ] = {
 ---------------------------------------------------------
 local KEY_DUN = QKey(748, 1, 200)
 
-QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_DUN ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER[ KEY_DUN ] = {
     { MonsterIndex = 14, Quantity = 10, MonsterName = 'Skeleton' },
 }
-
-QUEST_SYSTEM_REWARD_COINS[ KEY_DUN ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_ITEMS[ KEY_DUN ] = {
+    { ItemIndex = GET_ITEM(14, 180), Level = -1, Luck = -1, Skill = -1, Quantity = 5 },
+}
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_DUN ] = {
     { CoinName = 'Ruud', CoinAmount = 150, CoinIdentification = 4 },
 }
-QUEST_SYSTEM_REWARD_COINS[ KEY_DUN ] = {
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_DUN ] = {
     { CoinName = 'Zen', CoinAmount = 50000000, CoinIdentification = 5 },
 }
 ---------------------------------------------------------
@@ -180,30 +183,30 @@ QUEST_SYSTEM_REWARD_COINS[ KEY_DUN ] = {
 ---------------------------------------------------------
 local KEY_DVS = QKey(748, 2, 100)
 
-QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_DVS ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER[ KEY_DVS ] = {
     { MonsterIndex = 562, Quantity = 1, MonsterName = 'Dark Mammoth' },
     { MonsterIndex = 563, Quantity = 1, MonsterName = 'Dark Giant' },
     { MonsterIndex = 564, Quantity = 1, MonsterName = 'Dark Coolutin' },
     { MonsterIndex = 565, Quantity = 1, MonsterName = 'Dark Iron Knight' },
 }
 
-QUEST_SYSTEM_REWARD_COINS[ KEY_DVS ] = {
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_DVS ] = {
     { CoinName = 'Ruud', CoinAmount = 150, CoinIdentification = 4 },
 }
 
-QUEST_SYSTEM_REWARD_ITEMS[ KEY_DVS ] = {
+QUEST_SYSTEM_MAPS_REWARD_ITEMS[ KEY_DVS ] = {
     { ItemIndex = GET_ITEM(12,15), Level = 0, Op1=0, Op2=0, Life=0, Exc=0, Ancient=0, JoH=0, SockCount=0, ItemTime=0, DaysExpire=0, Flag=0, Name='Jewel Of Chaos x1', Count=1, Class = -1 }
 }
 
-QUEST_SYSTEM_REWARD_EXP[ KEY_DVS ] = {
+QUEST_SYSTEM_MAPS_REWARD_EXP[ KEY_DVS ] = {
     { ExpId = 3, Amount = 1, ExpName = "LVL UP" },
 }
 
-QUEST_SYSTEM_REWARD_BUFF[ KEY_DVS ] = {
+QUEST_SYSTEM_MAPS_REWARD_BUFF[ KEY_DVS ] = {
     { EffectID = 29, EffectTime = 3600, BuffName = 'Seal Ascencion' }
 }
 
-QUEST_SYSTEM_POINTS_REWARDS[ KEY_DVS ] = {
+QUEST_SYSTEM_MAPS_POINTS_REWARDS[ KEY_DVS ] = {
     {PtsID = 1, Amount = 10, StatName = "Free Points"},
 }
 
@@ -212,7 +215,7 @@ QUEST_SYSTEM_POINTS_REWARDS[ KEY_DVS ] = {
 ---------------------------------------------------------
 local KEY_KT = QKey(748, 37, 300)
 
-QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_KT ] = {
+QUEST_SYSTEM_MAPS_REQUIREMENTS_MONSTER[ KEY_KT ] = {
     { MonsterIndex = 353, Quantity = 5, MonsterName = 'Satyros' },
     { MonsterIndex = 354, Quantity = 5, MonsterName = 'Blade Hunter' },
     { MonsterIndex = 355, Quantity = 5, MonsterName = 'Kentauros' },
@@ -221,15 +224,15 @@ QUEST_SYSTEM_REQUIREMENTS_MONSTER[ KEY_KT ] = {
 	{ MonsterIndex = 357, Quantity = 5, MonsterName = 'Genocider' },
 }
 
-QUEST_SYSTEM_REWARD_COINS[ KEY_KT ] = {
+QUEST_SYSTEM_MAPS_REWARD_COINS[ KEY_KT ] = {
     { CoinName = 'Ruud', CoinAmount = 100, CoinIdentification = 4 },
 }
 
-QUEST_SYSTEM_REWARD_ITEMS[ KEY_KT ] = {
+QUEST_SYSTEM_MAPS_REWARD_ITEMS[ KEY_KT ] = {
     { ItemIndex = GET_ITEM(14,13), Level = 0, Op1=0, Op2=0, Life=0, Exc=0, Ancient=0, JoH=0, SockCount=0, ItemTime=0, DaysExpire=0, Flag=0, Name='Jewel Of Bless x1', Count=1, Class = -1 },
 }
 
-QUEST_SYSTEM_REWARD_EXP[ KEY_KT ] = {
+QUEST_SYSTEM_MAPS_REWARD_EXP[ KEY_KT ] = {
     { ExpId = 3, Amount = 1, ExpName = "LVL UP" },
 }
 ----------------
@@ -237,8 +240,8 @@ QUEST_SYSTEM_REWARD_EXP[ KEY_KT ] = {
 ----------------
 
 --Estos menjajes no tocarlos en el cliente son distintos.
-QUEST_SYSTEM_MESSAGES = {}
-QUEST_SYSTEM_MESSAGES['Eng'] = {
+QUEST_SYSTEM_MAPS_MESSAGES = {}
+QUEST_SYSTEM_MAPS_MESSAGES['Eng'] = {
     [1] = 'You are busy at the moment!',
     [2] = 'We havent found any quests available at the moment!',
     [3] = 'You have started quest %s!',
@@ -254,4 +257,4 @@ QUEST_SYSTEM_MESSAGES['Eng'] = {
     [21] = 'The mission has been completed!',
 	[22] = 'You already have an active quest with another NPC!',
 }
--- Fin QuestSystemConfig.lua (server)
+-- Fin QuestSystemByMapsConfig.lua (server)
